39c1eeb3c2bb6bff85d7b7cc118dbe8062656eb0
vleshenko <90086479+vleshenko@users.noreply.github.com>
2022-07-06 17:27:03 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,18 @@
-"use strict";
+// "use strict";
 
-let ulList = document.getElementById("ingredients");
-const foodIngredients = ingredients.forEach(ingredient => {
-  let items = document.createElement("li");
-  items.innerHTML = ingredient;
-  ulList.append(items);
-  parent.append(...items)
+// let ulList = document.getElementById("ingredients");
+// const foodIngredients = ingredients.forEach(ingredient => {
+//   let items = document.createElement("li");
+//   items.innerHTML = ingredient;
+//   ulList.append(items);
+//   parent.append(...items)
+// });
+
+// console.log(foodIngredients);
+
+const categoriesAnimal = document.querySelectorAll(".item");
+console.log(`Categories ${categoriesAnimal.length}`);
+categoriesAnimal.forEach((elem) => {
+  return console.log(`Category: ${elem.querySelector("h2").textContent}
+    Elements:${elem.querySelectorAll("li").length}`);
 });
-
-console.log(foodIngredients);
